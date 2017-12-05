579cc8a07fa6e5e285ce419c2603e88b48609cb6
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2017-12-05 01:28:06 +0000

    Put together rudimentary CLI option parsing
    Already 7 LANGUAGE extensions. Aren't we just doing great?

--- src/Tapal.hs
+++ src/Tapal.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+
 module Tapal
     ( someFunc
     ) where
 
+import Options.Generic as O
+
+data CliOptions = CliOptions { request :: String O.<?> "Path for the request specification"
+                             , scope :: Maybe String O.<?> "Scope in which the request is to be made"
+                             } deriving (Generic, Show)
+
+instance O.ParseRecord CliOptions
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  cliOptions <- O.getRecord @_ @CliOptions "Tapal, a lightweight command line alternative to Postman"
+  print cliOptions
