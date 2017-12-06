8cb7febae7912f775f4cbbf955d6142583d7e6f5
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2017-12-06 01:14:04 +0000

    Move CLI related code to `app` directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+
 module Main where
 
-import Tapal
+import Options.Generic as O
+
+data CliOptions = CliOptions { request :: String O.<?> "Path for the request specification"
+                             , scope :: Maybe String O.<?> "Scope in which the request is to be made"
+                             } deriving (Generic, Show)
+
+cliOptionModifiers :: O.Modifiers
+cliOptionModifiers = O.defaultModifiers { shortNameModifier = O.firstLetter }
+
+instance O.ParseRecord CliOptions where
+  parseRecord = parseRecordWithModifiers cliOptionModifiers
+
+cliApp :: IO ()
+cliApp = do
+  cliOptions <- O.getRecord @_ @CliOptions "Tapal, a lightweight command line alternative to Postman"
+  print cliOptions
 
 main :: IO ()
-main = someFunc
+main = cliApp
--- src/Tapal.hs
+++ src/Tapal.hs
@@ -1,28 +1,2 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE ExplicitNamespaces #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE PartialTypeSignatures #-}
+module Tapal where
 
-module Tapal
-    ( someFunc
-    ) where
-
-import Options.Generic as O
-
-data CliOptions = CliOptions { request :: String O.<?> "Path for the request specification"
-                             , scope :: Maybe String O.<?> "Scope in which the request is to be made"
-                             } deriving (Generic, Show)
-
-cliOptionModifiers :: O.Modifiers
-cliOptionModifiers = O.defaultModifiers { shortNameModifier = O.firstLetter }
-
-instance O.ParseRecord CliOptions where
-  parseRecord = parseRecordWithModifiers cliOptionModifiers
-
-someFunc :: IO ()
-someFunc = do
-  cliOptions <- O.getRecord @_ @CliOptions "Tapal, a lightweight command line alternative to Postman"
-  print cliOptions
