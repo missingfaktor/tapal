0ffaf97175d877924608106ab5c8cf103d9c1a68
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2017-12-11 12:55:51 +0530

    Parse the command to issue a request

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,6 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ExplicitNamespaces #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-
 module Main where
 
-import Options.Applicative as O
 import Tapal as T
 
-cliApp :: IO ()
-cliApp = do
-  cliOptions <- O.execParser T.cliOptionsParserInfo
-  print cliOptions
-
 main :: IO ()
-main = cliApp
+main = T.runTapal
--- app/Tapal.hs
+++ app/Tapal.hs
@@ -1,10 +1,3 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ExplicitNamespaces #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-
 module Tapal
   ( module Tapal
   ) where
@@ -12,22 +5,22 @@ module Tapal
 import Control.Applicative
 import Options.Applicative as O
 import Data.Monoid
+import Control.Monad.IO.Class
 
-data CliOptions = CliOptions { probe :: String
-                             , scope :: Maybe String
+newtype TapalCommand = Issue { requestPath :: String
                              } deriving (Show)
 
-cliOptionsParser :: O.Parser CliOptions
-cliOptionsParser =
-  CliOptions <$> O.strOption (O.long "probe"
-                           <> O.short 'p'
-                           <> O.help "Path for the probe specification" )
-             <*> optional (O.strOption (O.long "scope"
-                                     <> O.short 's'
-                                     <> O.help "Scope in which the request is to be made"))
+issueParser :: O.Parser TapalCommand
+issueParser = Issue <$> O.argument O.str (O.metavar "REQUEST_PATH")
 
-cliOptionsParserInfo :: O.ParserInfo CliOptions
-cliOptionsParserInfo =
-  O.info (cliOptionsParser <**> O.helper)
-         (O.fullDesc <> O.progDesc "Tapal, a lightweight command line alternative to Postman")
+tapalCommandParser :: O.Parser TapalCommand
+tapalCommandParser = O.hsubparser (O.command "issue" (O.info issueParser (O.progDesc "Issue a request")))
 
+tapalParserInfo :: O.ParserInfo TapalCommand
+tapalParserInfo = O.info (tapalCommandParser <**> O.helper)
+                         (O.fullDesc <> O.progDesc "Tapal, a lightweight command line alternative to Postman")
+
+runTapal :: MonadIO m => m ()
+runTapal = do
+  tapalCommand <- liftIO (O.execParser tapalParserInfo)
+  liftIO (print tapalCommand)
