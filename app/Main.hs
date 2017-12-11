3dbea06664d0d606d6b2d29cf17f7e3bb77c743c
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2017-12-11 11:31:41 +0530

    Eliminate probes

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,16 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ExplicitNamespaces #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 
 module Main where
 
-import Control.Applicative
 import Options.Applicative as O
 import Tapal as T
-import Data.Monoid
-
-data CliOptions = CliOptions { probe :: String
-                             , scope :: Maybe String
-                             } deriving (Show)
-
-cliOptionsParser :: O.Parser CliOptions
-cliOptionsParser =
-  CliOptions <$> O.strOption (O.long "probe"
-                           <> O.short 'p'
-                           <> O.help "Path for the probe specification" )
-             <*> optional (O.strOption (O.long "scope"
-                                     <> O.short 's'
-                                     <> O.help "Scope in which the request is to be made"))
-
-cliOptionsParserInfo :: O.ParserInfo CliOptions
-cliOptionsParserInfo =
-  O.info (cliOptionsParser <**> O.helper)
-         (O.fullDesc <> O.progDesc "Tapal, a lightweight command line alternative to Postman")
 
 cliApp :: IO ()
 cliApp = do
-  cliOptions <- O.execParser cliOptionsParserInfo
-  probe' <- T.probeAtDirectoryPath $ probe cliOptions
-  print probe'
+  cliOptions <- O.execParser T.cliOptionsParserInfo
   print cliOptions
 
 main :: IO ()
--- app/Tapal.hs
+++ app/Tapal.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+
 module Tapal
   ( module Tapal
-  , module Probe
   ) where
 
-import Probe
+import Control.Applicative
+import Options.Applicative as O
+import Data.Monoid
+
+data CliOptions = CliOptions { probe :: String
+                             , scope :: Maybe String
+                             } deriving (Show)
+
+cliOptionsParser :: O.Parser CliOptions
+cliOptionsParser =
+  CliOptions <$> O.strOption (O.long "probe"
+                           <> O.short 'p'
+                           <> O.help "Path for the probe specification" )
+             <*> optional (O.strOption (O.long "scope"
+                                     <> O.short 's'
+                                     <> O.help "Scope in which the request is to be made"))
+
+cliOptionsParserInfo :: O.ParserInfo CliOptions
+cliOptionsParserInfo =
+  O.info (cliOptionsParser <**> O.helper)
+         (O.fullDesc <> O.progDesc "Tapal, a lightweight command line alternative to Postman")
+
